c50da43ebca5c9b46e790951399e26171e43560a
LennonChin <i@coderap.com>
2017-10-30 23:53:34 +0800

    1, carousels touch slide;

--- weijinsuo/js/main.js
+++ weijinsuo/js/main.js
@@ -50,4 +50,38 @@ $(function () {
 
 	});
 
+	// 获取界面上的轮播图容器
+  var $carousels = $('.carousel');
+  var startX, endX;
+  var offset = 50;
+  // 注册滑动事件
+  $carousels.on('touchstart', function(e) {
+    // 手指触摸开始时记录一下手指所在的坐标X
+    startX = e.originalEvent.touches[0].clientX;
+    // console.log(startX);
+  });
+
+  $carousels.on('touchmove', function(e) {
+    // 变量重复赋值
+    endX = e.originalEvent.touches[0].clientX;
+    // console.log(endX);
+  });
+  $carousels.on('touchend', function(e) {
+    console.log(e);
+    // 结束触摸一瞬间记录最后的手指所在坐标X
+    // 比大小
+    // console.log(endX);
+    // 控制精度
+    // 获取每次运动的距离，当距离大于一定值时认为是有方向变化
+    var distance = Math.abs(startX - endX);
+    if (distance > offset) {
+      // 有方向变化
+      // console.log(startX > endX ? '←' : '→');
+      // 2. 根据获得到的方向选择上一张或者下一张
+      //     - $('a').click();
+      //     - 原生的carousel方法实现 http://v3.bootcss.com/javascript/#carousel-methods
+      $(this).carousel(startX > endX ? 'next' : 'prev');
+    }
+  });
+
 });
